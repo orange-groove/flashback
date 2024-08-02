2e00dd0271aaa8d909cc75f44c1e19468e0bf4b7
Adam Groves <agroves333@gmail.com>
2024-08-02 04:57:19 -0400

    up

--- src/components/PromoSection.tsx
+++ src/components/PromoSection.tsx
@@ -7,17 +7,18 @@ import { Promotion } from "@/types"
 import Link from "next/link"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import { slide } from "react-burger-menu"
 
 interface PromoSectionProps {
   promos: Promotion[]
 }
 
 export default function PromoSection({ promos }: PromoSectionProps) {
-  const slidesToShow = useBreakpointValue({
-    base: 1,
-    md: 2,
-    lg: 3,
-  })
+  const slidesToShow =
+    useBreakpointValue({
+      base: 1,
+      md: 3,
+    }) || 1
 
   return (
     <Flex
@@ -27,7 +28,13 @@ export default function PromoSection({ promos }: PromoSectionProps) {
       backgroundColor="black"
       color="white"
     >
-      <Box sx={{ width: ["100%", "50%"], m: "0 auto", textAlign: "center" }}>
+      <Box
+        sx={{
+          w: "100%",
+          m: "0 auto",
+          textAlign: "center",
+        }}
+      >
         <Heading
           as="h2"
           textStyle="h2"
@@ -40,14 +47,17 @@ export default function PromoSection({ promos }: PromoSectionProps) {
           Featured Events
         </Heading>
         <Slider
-          dots
-          infinite
           speed={500}
-          // slidesToShow={slidesToShow}
-          // slidesToScroll={slidesToShow}
+          slidesToShow={slidesToShow}
+          slidesToScroll={slidesToShow}
+          className="center"
+          adaptiveHeight
+          infinite
+          pauseOnHover
+          dots
         >
-          {promos?.map((promo) => (
-            <Link key={promo.id} href={promo.url || "#"} target="_blank">
+          {promos?.map((promo, key) => (
+            <Link key={`promo-${key}`} href={promo.url || "#"} target="_blank">
               <Image src={promo.image.url} alt={promo.title} />
             </Link>
           ))}
