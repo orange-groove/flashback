4fa02dfbdb57751c95648886b971aa71889e14dc
Adam Groves <agroves333@gmail.com>
2024-09-08 20:14:10 -0400

    add hours

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import {
   FaMapMarkerAlt as MapMarker,
   FaPhoneAlt as Phone,
 } from "react-icons/fa"
-import { Box, Flex, Heading, Link } from "@chakra-ui/react"
+import { Box, Divider, Flex, Heading, Link } from "@chakra-ui/react"
 import Map from "@/components/Map"
 
 const Footer = () => {
@@ -16,33 +16,34 @@ const Footer = () => {
       flexDirection="column"
       flexShrink={0}
       backgroundColor="white"
-      height={[800, 400]}
+      height={[800, 500]}
     >
       <Flex width="100%" height="100%" flexDirection={["column", "row"]}>
         <Box width="100%" height="100%">
           <Map />
         </Box>
         {/* Social icons and contact information */}
-        <Flex flexDirection="column" color="black" gap={2} p={8} lineHeight={1}>
-          <Heading as="h3" variant="script" size="xl" mb={2}>
-            Location
-          </Heading>
-          <Flex alignItems="top" gap={2}>
-            <MapMarker />
-            <Box>10010 B Ford Ave, Richmond Hill GA 31324</Box>
-          </Flex>
-          <Flex alignItems="top" gap={2}>
-            <Phone />
-            <Box>
-              <Link
-                href="tel:912.445.0119"
-                sx={{ textDecoration: "none", color: "black" }}
-              >
-                912.445.0119
-              </Link>
-            </Box>
-          </Flex>
-
+        <Flex flexDirection="column" color="black" gap={6} p={8} lineHeight={1}>
+          <Box>
+            <Heading as="h3" variant="script" size="xl">
+              Location
+            </Heading>
+            <Flex alignItems="top" gap={2}>
+              <MapMarker />
+              <Box>10010 B Ford Ave, Richmond Hill GA 31324</Box>
+            </Flex>
+            <Flex alignItems="top" gap={2}>
+              <Phone />
+              <Box>
+                <Link
+                  href="tel:912.445.0119"
+                  sx={{ textDecoration: "none", color: "black" }}
+                >
+                  912.445.0119
+                </Link>
+              </Box>
+            </Flex>
+          </Box>
           <Flex
             as="ul"
             sx={{
@@ -51,7 +52,6 @@ const Footer = () => {
               alignItems: "center",
               justifyContent: { base: "center", sm: "start" },
               gap: 2,
-              mt: "32px",
             }}
           >
             {/* Social icons go here */}
@@ -74,6 +74,22 @@ const Footer = () => {
               </Link>
             </li>
           </Flex>
+
+          <Box>
+            <Heading as="h3" variant="script" size="xl" mb={2}>
+              Hours
+            </Heading>
+            <Flex sx={{ flexDirection: "column", gap: 2 }}>
+              <Box>MON-THURS: 2:00PM - 12:00AM</Box>
+              <Box>KITCHEN CLOSES AT 10PM</Box>
+              <Box sx={{ borderBottom: "1px solid black" }} />
+              <Box>FRI-SAT: 11:30AM - 2:00AM</Box>
+              <Box>KITCHEN CLOSES AT 12:00AM</Box>
+              <Box sx={{ borderBottom: "1px solid black" }} />
+              <Box>SUN: 12:30PM - 12:00AM</Box>
+              <Box>KITCHEN CLOSES AT 10:PM</Box>
+            </Flex>
+          </Box>
         </Flex>
       </Flex>
     </Flex>
